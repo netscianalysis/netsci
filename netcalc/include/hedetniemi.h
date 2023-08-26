63050d4c98ba9e5de01a4f0f2d207b857d91037f
amstokely <amstokely@ucsd.edu>
2023-08-26 10:31:22 -0600

    fix: Added path recovery part of path finding algorithm to core hedetniemi kernel. This seems to have fixed the path recovery bug.

--- netcalc/include/hedetniemi.h
+++ netcalc/include/hedetniemi.h
@@ -26,6 +26,10 @@ namespace netcalc {
      *                  determine if a node belongs to the shortest path being calculated. If this value is too large,
      *                  the shortest path returned may be incorrect, and if it is too small, the algorithm may never terminate.
      *                  In all testing, 1e-5 has been a good value.
+     * \param maxPathLength The maximum number of nodes in a shortest
+     *                      allowed in a shortest path. The maximum
+     *                      number of edges allowed in a shortest path
+     *                      is equal to maxPathLength - 1.
      * \param platform  Platform used to perform the calculation. Use 0 for GPU and 1 for CPU.
      */
     void hedetniemiShortestPaths(
@@ -41,14 +45,22 @@ namespace netcalc {
      * \brief Compute the shortest paths and shortest path lengths between all pairs of nodes in a graph on the GPU.
      *
      * \param A         Adjacency matrix of the graph.
+     *
      * \param H         NxN matrix of shortest path lengths, where N is the number of nodes in the graph.
      *                  Each matrix element represents the shortest path length between the nodes corresponding
      *                  to the row and column indices of the element.
+     *
      * \param paths     Nx(N*k) matrix of shortest paths, where N is the number of nodes in the graph and k is
      *                  the number of nodes in the longest shortest path. Each row of the matrix contains the shortest
      *                  path between the node corresponding to the row index and all other nodes. Each path has k elements,
      *                  where the first element is the node corresponding to the row index and the last element is the node
      *                  corresponding to the column. Unless the path is the longest shortest path, the remaining elements are -1.
+     *
+     * \param maxPathLength The maximum number of nodes in a shortest
+     *                      allowed in a shortest path. The maximum
+     *                      number of edges allowed in a shortest path
+     *                      is equal to maxPathLength - 1.
+     *
      * \param tolerance A very small floating-point number (usually less than 1e-4) used in one of the algorithm steps to
      *                  determine if a node belongs to the shortest path being calculated. If this value is too large,
      *                  the shortest path returned may be incorrect, and if it is too small, the algorithm may never terminate.
@@ -101,6 +113,9 @@ namespace netcalc {
      *
      * \param paths     Nx(N*k) matrix of shortest paths, where N is the number of nodes in the graph and k is
      *                  the number of nodes in the longest shortest path.
+     * \param maxPathLength The maximum number of nodes in a shortest
+     *                      path, which is equal to the maximum number
+     *                      of edges allowed plus 1.
      * \param i         The index of the first node in the path.
      * \param j         The index of the last node in the path.
      */
