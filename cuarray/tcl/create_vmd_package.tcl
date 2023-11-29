a1b548b52dd95ffeb8bfe53e4101694e15fba18c
amstokely <amstokely@ucsd.edu>
2023-11-29 14:20:21 -0700

    feat: Added option to add tcl cuarray api to vmd tcl package list.

---
+++ cuarray/tcl/create_vmd_package.tcl
@@ -0,0 +1,4 @@
+exec echo load "$env(TCL_CUARRAY_LIB_PATH)" >> "$env(TCL_CUARRAY_SCRIPT_PATH)"
+exec echo load "$env(TCL_CUARRAY_LIB_PATH)" >> "$env(VMD_TCL_INDEX_PATH)"
+exec echo source "$env(TCL_CUARRAY_SCRIPT_PATH)" >> "$env(VMD_TCL_INDEX_PATH)"
+
---
+++ cuarray/tcl/cuarray.tcl
@@ -0,0 +1,65 @@
+proc loadFloatCuArray {fname} {
+    set floatCuArray [FloatCuArray]
+    $floatCuArray load $fname
+    return $floatCuArray
+}
+
+proc saveFloatCuArray {fname cuArray} {
+    $cuArray save $fname
+}
+
+proc argsortFloatCuArray {cuArray} {
+    set argsortCuArray [$cuArray argsort]}
+    return $argsortCuArray
+}
+
+proc sortFloatCuArray {cuArray} {
+    set sortedCuArray [$cuArray sort]}
+    return $sortedCuArray
+}
+
+proc floatCuArrayToList {cuArray} {
+    set a [list]
+    for {set i 0} {$i < [$cuArray m]} {incr i} {
+     set b [list]
+     for {set j 0} {$j < [$cuArray n]} {incr j} {
+            lappend b [$cuArray get $i $j]
+     }
+        lappend a $b
+    }
+    return $a
+}
+
+proc loadIntCuArray {fname} {
+    set intCuArray [IntCuArray]
+    $intCuArray load $fname
+    return $intCuArray
+}
+
+proc saveIntCuArray {fname cuArray} {
+    $cuArray save $fname
+}
+
+proc intCuArrayToList {cuArray} {
+    set a [list]
+    for {set i 0} {$i < [$cuArray m]} {incr i} {
+     set b [list]
+     for {set j 0} {$j < [$cuArray n]} {incr j} {
+            lappend b [$cuArray get $i $j]
+     }
+        lappend a $b
+    }
+    return $a
+}
+
+proc argsortIntCuArray {cuArray} {
+    set argsortCuArray [$cuArray argsort]}
+    return $argsortCuArray
+}
+
+proc sortIntCuArray {cuArray} {
+    set sortedCuArray [$cuArray sort]}
+    return $sortedCuArray
+}
+load /home/astokely/CLionProjects/netsci/cuarray/tcl/cuarray.so
+load /home/astokely/CLionProjects/netsci/cuarray/tcl/cuarray.so
